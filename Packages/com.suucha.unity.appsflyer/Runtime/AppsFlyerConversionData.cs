be816bbad3cdf85bd0e8a1791443adf977073fe3
tubo <tubo@weitown.com>
2023-09-10 13:44:31 +0800

    first commit

---
+++ Packages/com.suucha.unity.appsflyer/Runtime/AppsFlyerConversionData.cs
@@ -0,0 +1,88 @@
+﻿using AppsFlyerSDK;
+using SuuchaStudio.Unity.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuuchaStudio.Unity.AppsFlyers
+{
+    public delegate void ConversionDataSuccessHandler(Dictionary<string, object> conversionData);
+    public delegate void ConversionDataFailHandler(string error);
+    public delegate void AppOpenAttributionHandler(string data);
+    public delegate void AppOpenAttributionFailureHandler(string error);
+    public class AppsFlyerConversionData : SuuchaMonoBehaviourBase, IAppsFlyerConversionData
+    {
+        private event ConversionDataSuccessHandler ConversionDataSuccess;
+        public event ConversionDataSuccessHandler OnConversionDataSuccess
+        {
+            add
+            {
+                ConversionDataSuccess += value;
+            }
+            remove
+            {
+                ConversionDataSuccess -= value;
+            }
+        }
+
+        private event ConversionDataFailHandler ConversionDataFail;
+        public event ConversionDataFailHandler OnConversionDataFail
+        {
+            add { ConversionDataFail += value; }
+            remove { ConversionDataFail -= value; }
+        }
+
+        private event AppOpenAttributionHandler AppOpenAttribution;
+        public event AppOpenAttributionHandler OnAppOpenAttribution
+        {
+            add => AppOpenAttribution += value;
+            remove => AppOpenAttribution -= value;
+        }
+
+        private event AppOpenAttributionFailureHandler AppOpenAttributionFailure;
+        public event AppOpenAttributionFailureHandler OnAppOpenAttributionFailure
+        {
+            add => AppOpenAttributionFailure += value;
+            remove => OnAppOpenAttributionFailure -= value;
+        }
+
+        public void onAppOpenAttribution(string attributionData)
+        {
+            Logger.LogDebug("On app open attribution:" + attributionData);
+            AppOpenAttribution?.Invoke(attributionData);
+        }
+
+        public void onAppOpenAttributionFailure(string error)
+        {
+            Logger.LogError("On app open attribution failure:" + error);
+            AppOpenAttributionFailure?.Invoke(error);
+        }
+
+        public void onConversionDataFail(string error)
+        {
+            Logger.LogError("On conversion data fail:" + error);
+            ConversionDataFail?.Invoke(error);
+        }
+
+        public void onConversionDataSuccess(string conversionData)
+        {
+            Logger.LogDebug($"On Conversion data success:{conversionData}");
+            try
+            {
+                Dictionary<string, object> dictionary = AppsFlyer.CallbackStringToDictionary(conversionData);
+                dictionary.Add("Original", conversionData);
+                ConversionDataSuccess?.Invoke(dictionary);
+            }
+            catch (Exception ex)
+            {
+                Dictionary<string, object> dictionary2 = new Dictionary<string, object>();
+                dictionary2.Add("Original", conversionData);
+                ConversionDataSuccess?.Invoke(dictionary2);
+                Logger.LogError("On conversion data error:" + ex.Message + ", conversion data:" + conversionData);
+            }
+        }
+    }
+
+}
---
+++ Packages/com.suucha.unity.appsflyer/Runtime/AppsFlyerLogEventReporter.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AppsFlyerSDK;
+using System.Threading;
+using UnityEngine;
+using SuuchaStudio.Unity.Core.LogEvents;
+using SuuchaStudio.Unity.Core;
+using Cysharp.Threading.Tasks;
+
+namespace SuuchaStudio.Unity.LogEvents.AppsFlyers
+{
+    public class AppsFlyerLogEventReporter : LogEventReporterAbstract
+    {
+        private readonly bool onlyAfPurchaseWithRevenue = true;
+        public AppsFlyerLogEventReporter() : this(true)
+        {
+
+        }
+        public AppsFlyerLogEventReporter(bool onlyAfPurchaseWithRevenue)
+        {
+            this.onlyAfPurchaseWithRevenue = onlyAfPurchaseWithRevenue;
+        }
+        public AppsFlyerLogEventReporter(List<string> allowedEventNames,
+            List<string> excludedEventNames,
+            Dictionary<string, string> eventNameMap,
+            Dictionary<string, string> eventParameterNameMap)
+            : base(allowedEventNames, excludedEventNames, eventNameMap, eventParameterNameMap)
+        {
+            onlyAfPurchaseWithRevenue = true;
+        }
+        public override string Name => "AppsFlyer";
+
+        protected override UniTask LogEventInternal(string name, Dictionary<string, string> eventParameters)
+        {
+            if (onlyAfPurchaseWithRevenue && name != "af_purchase")
+            {
+                if (eventParameters.ContainsKey("af_revenue"))
+                {
+                    eventParameters.Remove("af_revenue");
+                }
+            }
+            Loom.QueueOnMainThread(() =>
+            {
+                AppsFlyer.sendEvent(name, eventParameters);
+            });
+            return UniTask.CompletedTask;
+        }
+
+    }
+
+    internal class Loom : SuuchaMonoBehaviourBase
+    {
+        public static int maxThreads = 8;
+        static int numThreads;
+
+        private static Loom _current;
+        private int _count;
+        public static Loom Current
+        {
+            get
+            {
+                Initialize();
+                return _current;
+            }
+        }
+        public Loom()
+        {
+
+        }
+
+        void Awake()
+        {
+            _current = this;
+            initialized = true;
+        }
+
+        static bool initialized;
+
+        static void Initialize()
+        {
+            if (!initialized)
+            {
+
+                if (!Application.isPlaying)
+                    return;
+                initialized = true;
+                var g = new GameObject("Loom");
+                DontDestroyOnLoad(g);
+                _current = g.AddComponent<Loom>();
+            }
+
+        }
+
+        private readonly List<Action> _actions = new List<Action>();
+        public struct DelayedQueueItem
+        {
+            public float time;
+            public Action action;
+        }
+        private readonly List<DelayedQueueItem> _delayed = new List<DelayedQueueItem>();
+        readonly List<DelayedQueueItem> _currentDelayed = new List<DelayedQueueItem>();
+
+        public static void QueueOnMainThread(Action action)
+        {
+            QueueOnMainThread(action, 0f);
+        }
+        public static void QueueOnMainThread(Action action, float time)
+        {
+            Current.Logger.LogDebug($"QueueOnMainThread entered.");
+            if (time != 0)
+            {
+                lock (Current._delayed)
+                {
+                    Current._delayed.Add(new DelayedQueueItem { time = Time.time + time, action = action });
+                }
+            }
+            else
+            {
+                lock (Current._actions)
+                {
+                    Current._actions.Add(action);
+                }
+            }
+        }
+
+        public static Thread RunAsync(Action a)
+        {
+            Initialize();
+            while (numThreads >= maxThreads)
+            {
+                Thread.Sleep(1);
+            }
+            Interlocked.Increment(ref numThreads);
+            ThreadPool.QueueUserWorkItem(RunAction, a);
+            return null;
+        }
+
+        private static void RunAction(object action)
+        {
+            try
+            {
+                ((Action)action)();
+            }
+            catch
+            {
+            }
+            finally
+            {
+                Interlocked.Decrement(ref numThreads);
+            }
+
+        }
+
+
+        void OnDisable()
+        {
+            if (_current == this)
+            {
+
+                _current = null;
+            }
+        }
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        readonly List<Action> _currentActions = new List<Action>();
+
+        // Update is called once per frame
+        void Update()
+        {
+            lock (_actions)
+            {
+                _currentActions.Clear();
+                _currentActions.AddRange(_actions);
+                _actions.Clear();
+            }
+            foreach (var a in _currentActions)
+            {
+                a();
+            }
+            lock (_delayed)
+            {
+                _currentDelayed.Clear();
+                _currentDelayed.AddRange(_delayed.Where(d => d.time <= Time.time));
+                foreach (var item in _currentDelayed)
+                    _delayed.Remove(item);
+            }
+            foreach (var delayed in _currentDelayed)
+            {
+                delayed.action();
+            }
+        }
+    }
+
+}
